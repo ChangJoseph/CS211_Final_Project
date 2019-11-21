cd12a9b526bdbe2dd76fa49910f4af2c893ae812
Yeoungmin Shin <scarh54@gmail.com>
2019-11-20 20:36:04 -0500

    added CalcLetterGrade method that returns letter grade with percentage

--- src/core/GPACalculator.java
+++ src/core/GPACalculator.java
@@ -1,3 +1,48 @@
 package core;
 
-public class GPACalculator {}
+public class GPACalculator extends Calculator {
+
+    private double percentage;
+
+    public GPACalculator(double percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public abstract float calculate() {
+
+    }
+    protected char CalcLetterGrade(double percentage) {
+        if (percentage => 4.00) {
+            return A;
+        }
+        else if (4.00 > percentage & percentage => 3.67) {
+            return A-;
+        }
+        else if (3.67 > percentage & percentage => 3.33) {
+            return B+;
+        }
+        else if (3.33 > percentage & percentage => 3.00) {
+            return B;
+        }
+        else if (3.00 > percentage & percentage => 2.67) {
+            return B-;
+        }
+        else if (2.67 > percentage & percentage => 2.33) {
+            return C+;
+        }
+        else if (2.33 > percentage & percentage => 2.00) {
+            return C;
+        }
+        else if (2.00 > percentage & percentage => 1.67) {
+            return C-
+        }
+        else if (1.67 > percentage & percentage => 1.00) {
+            return D;
+        }
+        else {
+            return F;
+        }
+    }
+
+}
