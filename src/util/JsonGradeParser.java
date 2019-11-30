412be84a7451a25e119923309cbf8d8ccfdfb4eb
Joseph Chang <niceinho@gmail.com>
2019-11-30 17:12:35 -0500

    Added more logic to parser

--- src/util/JsonGradeParser.java
+++ src/util/JsonGradeParser.java
@@ -12,11 +12,42 @@ public class JsonGradeParser {
         jsonManager = new BasicJsonManager(file);
     }
 
+    /**
+     * Parses the json file into 
+     * @return
+     */
     public List<Double> parse() {
-        List<Double> output = new ArrayList<Double>();
+        List<List<Double>> output = new ArrayList<List<Double>>();
         Scanner scan = jsonManager.getFileScanner();
+
         while (scan.hasNextLine()) {
             String currLine = scan.nextLine();
+            String manipulatedLine;
+            if (currLine.contains("{")) {
+                // manipulatedLine = currLine.substring(currLine.indexOf("{"));
+
+                while (!currLine.contains("}")) { // Run through whole json file
+
+                    if (currLine.contains("[")) {
+                        currLine = currLine.substring(1);
+                        while (!currLine.contains("]")) { // Run through each list in the object
+                            StringBuilder listContents = new StringBuilder();
+                            listContents.append("");
+                            if (scan.hasNextLine()) currLine = scan.nextLine();
+                            else break;
+                        }
+                    }
+                    if (!scan.hasNextLine()) break;
+                }
+            }
+            // else {
+            //     manipulatedLine = currLine;
+            // }
+            // String[] lineTokens = manipulatedLine.split(", ");
+            // if (lineTokens.length > 1) {
+                
+            // }
+            
         }
         // TODO implement parse code
         return output;
