76bbfef8c281b740d8c120bc69ab1add78301f7c
Hannah <hmarti3@gmu.edu>
2019-12-15 16:44:47 -0500

    test

--- src/test/java/core/ScaleCalculatorTest.java
+++ src/test/java/core/ScaleCalculatorTest.java
@@ -10,22 +10,33 @@ public class ScaleCalculatorTest {
     // TODO add tests lol
     @Test(timeout=1000)
     public void ScaleSizeTest() {
-       Scale assType = new Scale(0.25);
-       assType.add(88);
-       assType.add(90);
-       assType.add(78);
-       assType.add(85);
-       assType.total();
-        assertEquals("Whoopsies",4);  
+       Scale aType = new Scale(0.25f);
+       aType.add(88.0/100.0);
+       aType.add(90.0/100.0);
+       aType.add(78.0/100.0);
+       aType.add(85.0/100.0);
+       assertEquals("Incorrect, there is an issue with Size.", aType.total().equals(4),true);
+    }
+       
+       @Test(timeout=1000)
+    public void ScaleTest1() {
+       Scale typeA = new Scale(0.2f);
+       typeA.add(98.0/100.0);
+       typeA.add(99.0/100.0);
+       typeA.add(88.0/100.0);
+       typeA.add(99.0/100.0);
+       assertEquals("Incorrect, there is an issue with calculate.", typeA.calculate().equals(19.2),true);
     }
     
-    @Test(timeout=1000)
-    public void ScaleCalcTest1() {
-       Scale assType = new Scale(0.25);
-       assType.add(88);
-       assType.add(90);
-       assType.add(78);
-       assType.add(85);
-       assType.calculate();
-       assertEquals("Whoopsies",21.3);
+       @Test(timeout=1000)
+    public void ScaleTest2() {
+       Scale typeB = new Scale(0.25f);
+       typeB.add(88.0/100.0);
+       typeB.add(90.0/100.0);
+       typeB.add(78.0/100.0);
+       typeB.add(85.0/100.0);
+       typeB.calculate();
+       assertEquals("Incorrect, there is an issue with calculate.", typeB.calculate().equals(21.3),true);
+}
+    
 }
